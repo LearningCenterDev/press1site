e764c33e8e24d76458c12b2d097a82c0ca323520
LearningCenterDev <learningcenterdeveloper@gmail.com>
2026-01-14 17:31:16 +0545

    Fix formatting issues in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createRoot } from 'react-dom/client'
 
+
+
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
