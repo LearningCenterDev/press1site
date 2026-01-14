3c4a32738bc446cb84841b5136f7bc741ad1aa31
Sampannasapkota <henryatokpas12@gmail.com>
2026-01-14 17:37:28 +0545

    refactor: simplify StrictMode usage in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,10 +6,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <StrictMode>
-      <App />
-    </StrictMode>,
+    <App />
   </StrictMode>
 )
 
