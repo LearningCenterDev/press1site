4d40aa51ac42afda974a37657c5f7e07577272ba
LearningCenterDev <learningcenterdeveloper@gmail.com>
2026-01-14 17:33:30 +0545

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
-
-
-import React from 'react'
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { createRoot } from 'react-dom/client'
-
-
-
-
-
-
 
 createRoot(document.getElementById('root')).render(
-  <React>
+  <StrictMode>
     <App />
-  </React>
+  </StrictMode>,
 )
+
