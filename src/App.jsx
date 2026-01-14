9d19606197a004eac8d314d3e05c2dc49762188e
Sampannasapkota <henryatokpas12@gmail.com>
2026-01-14 17:36:35 +0545

    refactor: remove unused imports and clean up component structure in App and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import { useState } from 'react'
+
 import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Contact from './components/Contact'
-import Services from './components/Services'
-import About from './components/About'
-import Footer from './components/Footer'
-import Portfolio from './components/Portfolio'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Career from './pages/Career'
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/career" element={<Career />} />
-        </Routes>
-      </BrowserRouter>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/career" element={<Career />} />
+      </Routes>
+    </BrowserRouter>
 
 
-    </>
+
   )
 }
 
